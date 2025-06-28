2e65058f474987fd96602b40d4dd2e7565d33764
ifsabhi64 <ifsabhi64@gmail.com>
2025-06-28 12:38:12 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@ import os
 
 api_id = 20567114
 api_hash = os.environ.get("API_HASH", "8a5b92106e45fc6637a65a67df060a65")
-bot_token = os.environ.get("7863736606:AAFNyv9PxoAh46PrIAcAl-SbbcvpLcfxNdU")
+bot_token = os.environ.get("8090907579:AAHiZQi_PcGhs99F5e6EglZW3JVmdc-yTZo")
 auth_users = os.environ.get("AUTH_USERS", "8036182138")
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = os.environ.get("OWNER_USERS", "8036182138")
